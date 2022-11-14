105c8878e77732ef06909b29a6369819a464a3e2
Kirill Murzin <murz.kirill@yandex.ru>
2022-11-14 15:57:18 +0500

    lab2: socket

--- lab2/utils/services/SocketService.go
+++ lab2/utils/services/SocketService.go
@@ -2,20 +2,27 @@ package services
 
 import (
 	"bytes"
-	"crypto/rand"
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
 	"lab2/utils/dto"
 	"net"
+	"os"
 )
 
 func PutUnnormalizedDataToSocket(unnormalizedStudents []dto.UnnormalizedStudent) {
-	cert, err := tls.LoadX509KeyPair("../certs/client_v1234281.hosted-by-vdsina.ru_certificate.pem", "../certs/client_v1234281.hosted-by-vdsina.ru_key.pem")
-	failOnError(err, "Failed to load keys")
-	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
-	conn, err := tls.Dial("tcp", "176.124.200.41:9876", &config)
+	caCert, err := os.ReadFile("../certs/ca_certificate.pem")
+	failOnError(err, "Failed to open ca cert")
+	cert, err := tls.LoadX509KeyPair("../certs/client_v1234281.hosted-by-vdsina.ru_certificate.pem",
+		"../certs/client_v1234281.hosted-by-vdsina.ru_key.pem")
+	failOnError(err, "Failed to load keypair")
+	rootCas := x509.NewCertPool()
+	rootCas.AppendCertsFromPEM(caCert)
+	tlsConf := &tls.Config{RootCAs: rootCas,
+		Certificates: []tls.Certificate{cert}}
+	conn, err := tls.Dial("tcp", "176.124.200.41:9876", tlsConf)
 	failOnError(err, "Failed to connect to socket")
 	defer conn.Close()
 
@@ -29,12 +36,17 @@ func PutUnnormalizedDataToSocket(unnormalizedStudents []dto.UnnormalizedStudent)
 }
 
 func GetUnnormalizedDataFromSocket() {
+	caCert, err := os.ReadFile("../certs/ca_certificate.pem")
+	failOnError(err, "Failed to open ca cert")
 	cert, err := tls.LoadX509KeyPair("../certs/client_v1234281.hosted-by-vdsina.ru_certificate.pem",
 		"../certs/client_v1234281.hosted-by-vdsina.ru_key.pem")
-	failOnError(err, "Failed to load keys")
-	config := tls.Config{Certificates: []tls.Certificate{cert}}
-	config.Rand = rand.Reader
-	listener, err := tls.Listen("tcp", "localhost:9876", &config)
+	failOnError(err, "Failed to load keypair")
+	rootCas := x509.NewCertPool()
+	rootCas.AppendCertsFromPEM(caCert)
+	tlsConf := &tls.Config{RootCAs: rootCas,
+		Certificates: []tls.Certificate{cert}}
+
+	listener, err := tls.Listen("tcp", "localhost:9876", tlsConf)
 
 	for {
 		conn, err := listener.Accept()
