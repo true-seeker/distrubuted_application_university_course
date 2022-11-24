879c8c52504e06f156d8ded9c7b7667b572170ad
Kirill Murzin <murz.kirill@yandex.ru>
2022-11-24 08:12:43 +0500

    lab2: clean

--- lab2/certs/ssl.sh
+++
@@ -1,7 +0,0 @@
-openssl genrsa -out RMQ-CA-Key.pem
-openssl req -new -key RMQ-CA-Key.pem -x509 -days 100 -out RMQ-CA-cert.pem
-openssl genrsa -out RMQ-server-key.pem
-openssl req -new -config /etc/ssl/openssl.cnf -key RMQ-server-key.pem -out RMQ-signingrequest.csr
-openssl x509 -req -days 100 -in RMQ-signingrequest.csr -CA RMQ-CA-cert.pem -CAkey RMQ-CA-Key.pem -CAcreateserial -out RMQ-server-cert.pem
-#cat RMQ-server-key.pem RMQ-server-cert.pem > RMQ-serverpemkeyfile.pem
-
